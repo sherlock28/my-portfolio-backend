8d48e1788da461249eeaa86704dfca2650ce4ab3
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-04-12 06:57:56 -0300

    User model is added

---
+++ src/models/User.model.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: String,
+  email: String,
+  password: String,
+  token: String,
+});
+
+module.exports = model("User", UserSchema);
