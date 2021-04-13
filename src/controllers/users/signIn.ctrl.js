42e655f8f4fb6c731962726126ad78a7a0fd331c
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-04-12 22:16:11 -0300

    User sign in controller is added

--- src/controllers/users/signIn.ctrl.js
+++ src/controllers/users/signIn.ctrl.js
@@ -1,21 +1,49 @@
-const { User } = require('../../models');
+const { User } = require("../../models");
 const { validatePassword } = require("../../libs");
 const jwt = require("jsonwebtoken");
 
 const signIn = async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
+  const user = await User.findOne({ email: req.body.email });
   if (user === null) {
-      res.status(400).json({
-          message: 'Invalid credentials'
-      });
+    res.status(400).json({
+      message: "Invalid credentials",
+    });
   } else {
-      console.log(user)
-    //   const isCorrectPass = await validatePassword();
-    //   if(!isCorrectPass) {
+    const isCorrectPass = await validatePassword(
+      req.body.password,
+      user.password
+    );
 
-    //   }
+    if (isCorrectPass) {
+      const token = await jwt.sign(
+        {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: 86400 }
+      );
+
+      const query = { email: user.email };
+      await User.findOneAndUpdate(query, { token: token });
+
+      res
+        .status(200)
+        .header({
+          Authorization: token,
+          "Access-Control-Expose-Headers": "Authorization",
+        })
+        .json({
+          status: "Ok",
+          message: "You are authenticated",
+        });
+    } else {
+      res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
   }
-  res.json({ msg: "signIn" });
 };
 
 module.exports = signIn;
--- src/database.js
+++ src/database.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URI_DB, {
     useNewUrlParser: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(db => console.log("db is connected"))
   .catch(err => console.error(err));
