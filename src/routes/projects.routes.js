4a2ca5a0355103f6bc2f7d95b429ec40b1c59b8f
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-05-09 23:56:43 -0300

    Correction of middleware used in project endpoints

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -2,22 +2,22 @@ const router = require("express").Router();
 const { users } = require("../middlewares");
 const { projectsCtrl } = require("../controllers");
 
-router.get("/projects", users.fieldUserValidation, projectsCtrl.getProjects);
+router.get("/projects", users.tokenValidation, projectsCtrl.getProjects);
 router.get("/projects/public", projectsCtrl.getPublicProjects);
 router.get(
   "/projects/:id_project",
-  users.fieldUserValidation,
+  users.tokenValidation,
   projectsCtrl.getProjectById
 );
-router.post("/projects", users.fieldUserValidation, projectsCtrl.postProject);
+router.post("/projects", users.tokenValidation, projectsCtrl.postProject);
 router.delete(
   "/projects/:id_project",
-  users.fieldUserValidation,
+  users.tokenValidation,
   projectsCtrl.deleteProject
 );
 router.put(
   "/projects/:id_project",
-  users.fieldUserValidation,
+  users.tokenValidation,
   projectsCtrl.putProjects
 );
 
